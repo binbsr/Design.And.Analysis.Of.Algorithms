9dee92ee506a63ba5067e502b273f9318a2cd7f8
Bhawana Rawal <57823412+bhwbi@users.noreply.github.com>
2019-12-30 08:32:10 +0545

    Added iterative fib

---
+++ Iterative/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Iterative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nth = 90;
+            int fibNum = Fib_Recursive(nth);
+            Console.WriteLine(nth + "th fibonacci number: " + fibNum);
+        }
+
+        static int Fib_Recursive(int n)
+        {
+            if (n == 1)
+                return 0;
+
+            if (n == 2)
+                return 1;
+
+            return Fib_Recursive(n - 1) + Fib_Recursive(n - 2);
+        }
+    }
+}
