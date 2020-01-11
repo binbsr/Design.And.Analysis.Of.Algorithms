eeb50510ecf1d94e95ebbf6cdb269138f5986758
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-01-11 13:17:56 +0545

    Added basic sorting testbench and refactoring

---
+++ Iterative/BasicSorting_TB.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace Iterative
+{
+    public class BasicSorting_TB
+    {
+        public static void Print_SortedItems_And_Steps_Table(int from = 1, int to = 100000)
+        {
+            Console.WriteLine("############### Basic Sorting: Bubble, Selection, Insertion ################");
+            Console.WriteLine("############################################################################");
+            Console.WriteLine("1. Random Arrays\n");
+            Console.WriteLine($"{"Array Size", 15}{"Bubble Steps", 20}{"Selection Steps", 20}{"Insertion Steps", 20}");            
+            for (int i = from; i <= to; i = i * 10)
+            {
+                BasicSorting.StepsBubble = 0;
+                BasicSorting.StepsSelection = 0;               
+                var items = GenerateRandomArray(i);
+                BasicSorting.RunBubbleSort(items);
+                BasicSorting.RunSelectionSort(items);
+                Console.Write($"{i, 15:N0}{BasicSorting.StepsBubble, 20:N0}{BasicSorting.StepsSelection, 20:N0}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("2. Sorted Arrays\n");
+            Console.WriteLine($"{"Array Size", 15}{"Bubble Steps", 20}{"Selection Steps", 20}{"Insertion Steps", 20}");            
+            for (int i = from; i <= to; i = i * 10)
+            {
+                BasicSorting.StepsBubble = 0;
+                BasicSorting.StepsSelection = 0;
+                var items = GenerateRandomArray(i, sorted: true);
+                BasicSorting.RunBubbleSort(items);
+                BasicSorting.RunSelectionSort(items);
+                Console.Write($"{i, 15:N0}{BasicSorting.StepsBubble, 20:N0}{BasicSorting.StepsSelection, 20:N0}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("3. Reverse Sorted Arrays\n");
+            Console.WriteLine($"{"Array Size", 15}{"Bubble Steps", 20}{"Selection Steps", 20}{"Insertion Steps", 20}");            
+            for (int i = from; i <= to; i = i * 10)
+            {
+                BasicSorting.StepsBubble = 0;
+                BasicSorting.StepsSelection = 0;
+                var items = GenerateRandomArray(i, sorted: true, ascending: false);
+                BasicSorting.RunBubbleSort(items);
+                BasicSorting.RunSelectionSort(items);
+                Console.Write($"{i, 15:N0}{BasicSorting.StepsBubble, 20:N0}{BasicSorting.StepsSelection, 20:N0}");
+                Console.WriteLine();
+            }
+        }
+
+        private static int[] GenerateRandomArray(int n, bool sorted = false, bool ascending = true)
+        {
+            Random r = new Random();
+            int[] items = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                items[i] = r.Next(1, n);
+            }
+
+            if(sorted)
+            {
+                // Using built-in sorting algorithms
+                if(ascending)
+                    Array.Sort(items);
+                else
+                    Array.Reverse(items);
+            }           
+
+            return items;
+        }
+    }
+}
--- Iterative/Program.cs
+++ Iterative/Program.cs
@@ -4,64 +4,12 @@ namespace Iterative
 {
     public class Program
     {
-        static long counter_recursive = 0;
-        static long counter_iterative = 0;
-
         static void Main(string[] args)
         {
-            int from = 3;
-            int to = 20;
-
-            Console.WriteLine("############### Fibonacci Iterative ################");            
-            for (int i = from; i <= to; i++)
-            {
-                counter_iterative = 0;
-                Console.WriteLine($"{i}th Fib: {Fib_Iterative(i)}, Steps: {counter_iterative}");
-            }
-
-            Console.WriteLine("############### Fibonacci Recursive ################");
-
-            for (int i = from; i <= to; i++)
-            {
-                counter_recursive = 0;
-                Console.WriteLine($"{i}th Fib: {Fib_Recursive(i)}, Steps: {counter_recursive}");
-            }
-        }
-
-        public static int Fib_Recursive(int n)
-        {
-            counter_recursive++;
-            if (n == 1)
-                return 0;
-            if (n == 2)
-                return 1;
-
-            return Fib_Recursive(n - 1) + Fib_Recursive(n - 2);
-        }
-        public static long Fib_Iterative(int n)
-        {
-            long[] fibs = new long[n];
-
-            fibs[0] = 0;
-            fibs[1] = 1;
-
-            for (int i = 2; i < n; i++)
-            {
-                counter_iterative++;
-                fibs[i] = fibs[i - 1] + fibs[i - 2];
-            }
-
-            return fibs[n - 1];
-        }
-
-        public static int GCD_Recursive(int a, int b)
-        {
-            return 0;
-        }
-
-        public static int GCD_Iterative(int a, int b)
-        {
-            return 0;
+            //Fibonacci_TB.Print_NthFib_And_Steps_Table();  
+            //GCD_TB.Print_GCD_And_Steps_Table(3135, 3100);  
+            BasicSorting_TB.Print_SortedItems_And_Steps_Table(1, 10000);
+            //var sortedNumbers = BasicSorting.RunInsertionSort(new int[] { 3, 2, 5, 7, 4, 0, 1, 34, 22});
         }
     }
 }
